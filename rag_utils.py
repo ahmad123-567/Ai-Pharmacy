fa3e4059eed31de23c75b18bf709a0810fb5ffc3
ahmad123-567 <ahmadjutt07795@gmail.com>
2026-09-08 20:52:51 +0500

    Update rag_utils.py

--- rag_utils.py
+++ rag_utils.py
@@ -19,7 +19,7 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import FAISS
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_groq import ChatGroq
-from langchain.docstore.document import Document
+from langchain_core.documents import Document
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain.chains import create_retrieval_chain
 from langchain_core.prompts import ChatPromptTemplate
