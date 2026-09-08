7838e11861240be6b7d8353308a870abbc1ba76a
ahmad123-567 <ahmadjutt07795@gmail.com>
2026-09-08 21:00:18 +0500

    Update rag_utils.py

--- rag_utils.py
+++ rag_utils.py
@@ -15,14 +15,20 @@ import io
 from typing import List
 
 from pypdf import PdfReader
+
+
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import FAISS
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_groq import ChatGroq
+
 from langchain_core.documents import Document
-from langchain.chains.combine_documents import create_stuff_documents_chain
-from langchain.chains import create_retrieval_chain
 from langchain_core.prompts import ChatPromptTemplate
+
+from langchain_classic.chains.combine_documents import (
+    create_stuff_documents_chain
+)
+from langchain_classic.chains import create_retrieval_chain
 
 
 # ---------------------------------------------------------------------------
